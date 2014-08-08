e1c3c9c0f639f7f38523bbd8cec65ecdd278fb7f
Michal Koutný <xm.koutny@gmail.com>
2014-08-08 17:43:18 +0200

    xcs-configure more robust

--- xcs-configure.sh
+++ xcs-configure.sh
@@ -1,13 +1,17 @@
+#!/bin/bash
+
 #
 # See: http://belion.tumblr.com/post/36151777927/ptam-compilation-notes
 #
 
-TooN_ROOT="$PWD/../../Build/TooN"
+DIR="`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`"
+TooN_ROOT="$DIR/../../Build/TooN"
+
 export CPPFLAGS="-D_REENTRANT -I$TooN_ROOT/include"
 export CXXFLAGS="-Wno-unused-variable -fomit-frame-pointer"
 export LDFLAGS=-L$TooN_ROOT/lib
 
 ./configure --without-ffmpeg \
-   --prefix=$PWD/../../Build/libcvd \
+   --prefix=$DIR/../../Build/libcvd \
    --without-v4l1buffer \
    --with-linux=`uname -r`
